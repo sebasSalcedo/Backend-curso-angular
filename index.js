3dc6015849cde28c75fcb43e8c2c0db8a3578ddf
sebasSalcedo <vastiansalcedo97@gmail.com>
2022-07-11 12:55:57 -0500

    Desarrollo del servidor

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { dbConnection } = require("./database/config");
+
+
+// Crear el servidor de express
+
+const app = express();
+
+// Configurar CORS
+
+app.use(cors());   
+
+// Base de datos
+
+dbConnection();
+
+// Rutas
+
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+    msg: "Hola Mundo",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Servidor corriendo en puerto", +process.env.PORT);
+});
